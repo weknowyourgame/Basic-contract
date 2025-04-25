90c9cddda6a5c7c7c49efb7530e441e8cbcdc625
Sarthak2 <aniki123.ai@gmail.com>
2025-04-25 06:07:58 +0530

    counter program

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,42 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-        account_info::{next_account_info, AcoountInfo},
-        entrypoint::ProgmramResult,
-        msg,
-        pubkey::Pubkey,
-        entrypoint
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
 };
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Counter {
+    pub count: u64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub enum CounterInstruction {
+    Increament(u32),
+    Decreament(u32),
+}
 
 pub fn counter_program(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8]
+    program_id: &Pubkey,                          // address of the program users are supposed to specify this when they interact with this contract
+    accounts: &[AccountInfo],                    // an array of input addys the user uses to interact with they call this func 
+    instruction_data: &[u8],                    // the actual thing the user wants to do --> contains info like what to do (like add to a counter, remove to a counter)
 ) -> ProgramResult {
-    let acnt = next_account_info(&mut accounts.iter()).unwrap();
-};
+    let account_info_iter = &mut accounts.iter();
+    let account = next_account_info(account_info_iter)?;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+    let mut counter = Counter::try_from_slice(&account.data.borrow())?;
+
+    match CounterInstruction::try_from_slice(instruction_data)? {
+        CounterInstruction::Increament(amount) => {
+            counter.count += amount;
+        }
+        CounterInstruction::Decreament(amount) => {
+            counter.count -= amount;
+        }
     }
+
+    counter.serialize(&mut &mut account.data.borrow_mut())?;
+    Ok(())
 }
+
